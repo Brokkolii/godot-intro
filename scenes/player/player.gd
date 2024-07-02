57cf39ff8761b7007e7add9eb2592348e3709d9c
Patrick Girnghuber <p.girnghuber@icloud.com>
2024-07-03 00:12:43 +0200

    basic camera

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,10 +3,10 @@ extends CharacterBody2D
 const speed: int = 500
 
 var can_laser: bool = true
-signal laser_fired(pos)
+signal laser_fired(pos, dir)
 
 var can_grenade: bool = true
-signal grenade_thrown(pos)
+signal grenade_thrown(pos, dir)
 
 func _process(_delta):
 	
@@ -14,19 +14,24 @@ func _process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down");
 	velocity = direction * speed
 	move_and_slide()
+	
+	# rotate
+	look_at(get_global_mouse_position())
 
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	# laser input
 	if Input.is_action_pressed("primary action") and can_laser:
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers.pick_random()
-		laser_fired.emit(selected_laser.global_position)
+		laser_fired.emit(selected_laser.global_position, player_direction)
 		can_laser = false
 		$CanLaserTimer.start()
 	
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		var grenade_markers = $GrenadeStartPosition.get_children()
 		var selected_grenade = grenade_markers.pick_random()
-		grenade_thrown.emit(selected_grenade.global_position)
+		
+		grenade_thrown.emit(selected_grenade.global_position, player_direction)
 		can_grenade = false
 		$CanGrenadeTimer.start()
 		
